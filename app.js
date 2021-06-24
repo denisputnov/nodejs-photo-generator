38568c51ed0f7783e7e5249c44573c06c744cd40
Денис Путнов <grnbows.connect@mail.ru>
2021-06-24 15:44:00 +0400

    bot interface updated

--- app.js
+++ app.js
@@ -9,24 +9,6 @@ let header = ''
 let text = ''
 
 bot.on('message', async (message) => {
-  if (command(message, '/set_header')) {
-    const candidate = message.text.split(' ').slice(1).join(' ')
-    if (candidate.length < 1 || candidate.length > 36) {
-      return bot.sendMessage(message.chat.id, `length: ${candidate.length}\nНе соответствует лимитам длины`)
-    }
-    header = candidate
-    return bot.sendMessage(message.chat.id, `header: ${header}`)
-  } 
-  
-  if (command(message, '/set_text')) {
-    const candidate = message.text.split(' ').slice(1).join(' ')
-    if (candidate.length < 10 || candidate.length > 115) {
-      return bot.sendMessage(message.chat.id, `length: ${candidate.length}\nНе соответствует лимитам длины`)
-    }
-    text = candidate
-    return bot.sendMessage(message.chat.id, `text: ${text}`)
-  }
-
   if (command(message, '/info')) {
     return bot.sendMessage(message.chat.id, (header.length || text.length) ? `${header}\n\n${text}` : 'Пусто')
   } 
@@ -34,6 +16,16 @@ bot.on('message', async (message) => {
   if (command(message, '/generate')) {
     await generate(header, text)
     return bot.sendPhoto(message.chat.id, './out.png')
+  }
+
+  if (message.text.length > 1 && message.text.length <= 36) {
+    header = message.text
+    return bot.sendMessage(message.chat.id, (header.length || text.length) ? `${header}\n\n${text}` : 'Пусто')
+  }
+
+  if (message.text.length > 36 && message.text.length <= 115) {
+    text = message.text
+    return bot.sendMessage(message.chat.id, (header.length || text.length) ? `${header}\n\n${text}` : 'Пусто')
   }
 })
 
