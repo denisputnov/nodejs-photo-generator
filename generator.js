78feb457944808c020c0c6d622a85f3599dceada
Денис Путнов <grnbows.connect@mail.ru>
2021-06-24 22:39:08 +0400

    styles fixed

--- generator.js
+++ generator.js
@@ -12,35 +12,36 @@ module.exports = async function generate(header, text) {
     <html>
       <head>
         <style>
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: Consolas, monaco, monospace; 
-            color: white
-          }
-          body {
-            padding: 7vw;
-            width: 5120px;
-            height: 2048px;
-          }
-          h1 {
-            font-size: 180px;
-            margin-bottom: 80px;
-            font-weight: 600;
-          }
-  
-          p {
-            font-size: 180px;
-          }
-  
-          img {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: -1;
-          }
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: Consolas, monaco, monospace; 
+          color: white
+        }
+        body {
+          padding: 7vw;
+          width: 5120px;
+          height: 2048px;
+        }
+        h1 {
+          font-size: 180px;
+          margin-bottom: 140px;
+          font-weight: 600;
+        }
+
+        p {
+          font-size: 180px;
+          line-height: 240px;
+        }
+
+        img {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          z-index: -1;
+        }
         </style>
       </head> 
       <body>
